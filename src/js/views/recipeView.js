1d364afad179c1523d0f5f90ea8e661c1d58303b
vitorgaletti <vitor.fgaletti@gmail.com>
2021-08-24 16:58:30 -0300

    Bug fraction

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from 'url:../../img/icons.svg'; // Parcel 2
-import { Fraction } from 'fraction.js';
+import { Fraction } from 'fractional';
 
 class RecipeView extends View {
   _parentElement = document.querySelector('.recipe');

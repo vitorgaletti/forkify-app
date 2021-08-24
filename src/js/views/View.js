26c11c600fa610aa7ac4c4ec3012d666e271d6fe
vitorgaletti <vitor.fgaletti@gmail.com>
2021-08-24 16:36:05 -0300

    Bug fraction

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -85,14 +85,14 @@ export default class View {
 
   renderMessage(message = this._message) {
     const markup = `
-          <div class="message">
-            <div>
-              <svg>
-                <use href="${icons}#icon-smile"></use>
-              </svg>
-            </div>
-            <p>${message}</p>
-          </div> 
+      <div class="message">
+        <div>
+          <svg>
+            <use href="${icons}#icon-smile"></use>
+          </svg>
+        </div>
+        <p>${message}</p>
+      </div>
     `;
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);

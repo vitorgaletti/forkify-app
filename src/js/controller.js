5afc6ae0db9ac9e9115122608d82c6e4e4d05d5f
vitorgaletti <vitor.fgaletti@gmail.com>
2021-08-23 21:59:34 -0300

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -126,6 +126,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -134,6 +138,7 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 
 init();
